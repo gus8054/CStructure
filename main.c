cdd2ed1195313ee21bb81a5bfc513108b37a1b30
gus8054 <gus8054@kakao.com>
2025-05-03 16:43:34 +0900

    REFACTORY: arraystack.c file, createArrayStack() and popAS()

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 // #include "list/arraylist/arraylisttest.c"
 // #include "list/linkedlist/linkedlisttest.c"
 // #include "list/circularlist/circularlisttest.c"
-#include "list/doublylist/doublylisttest.c"
-// #include "stack/arraystack/arraystacktest.c"
+// #include "list/doublylist/doublylisttest.c"
+#include "stack/arraystack/arraystacktest.c"
 // #include "stack/linkedstack/linkedstacktest.c"
 // #include "queue/arrayqueue/arrayqueuetest.c"
 // #include "queue/linkedqueue/linkedqueuetest.c"
@@ -13,8 +13,8 @@ int main() {
   // arrayListMain();
   // linkedListMain();
   // circularListMain();
-  doublyListMain();
-  // arrayStackMain();
+  // doublyListMain();
+  arrayStackMain();
   // linkedStackMain();
   // arrayQueueMain();
   // linkedQueueMain();
--- stack/arraystack/arraystack.c
+++ stack/arraystack/arraystack.c
@@ -1,31 +1,30 @@
 #include "arraystack.h"
 
 ArrayStack* createArrayStack(int maxElementCount){
-    if(maxElementCount <= 0 ) return NULL;
+    if(maxElementCount <= 0) return NULL;
     ArrayStack* pReturn = (ArrayStack*)malloc(sizeof(ArrayStack));
     if(pReturn == NULL) return NULL;
     pReturn->maxElementCount = maxElementCount;
     pReturn->currentElementCount = 0;
-    pReturn->pElement = (ArrayStackNode*)malloc(sizeof(ArrayStackNode) * maxElementCount);
-    if(pReturn->pElement == NULL){
+    ArrayStackNode* pElement = (ArrayStackNode*)malloc(sizeof(ArrayStackNode) * maxElementCount);
+    if(pElement == NULL){
         free(pReturn);
         return NULL;
     }
-    memset(pReturn->pElement, 0, sizeof(ArrayStackNode) * maxElementCount);
+    memset(pElement, 0, sizeof(ArrayStackNode) * maxElementCount);
+    pReturn->pElement = pElement;
     return pReturn;
 }
 
 void deleteArrayStack(ArrayStack* pStack){
     if(pStack == NULL) return;
-    if(pStack->pElement != NULL)
-        free(pStack->pElement);
+    if(pStack->pElement != NULL) free(pStack->pElement);
     free(pStack);
 }
 
 bool pushAS(ArrayStack* pStack, ArrayStackNode element){
     if(pStack == NULL) return false;
-    if(isArrayStackFull(pStack)) return false;
-
+    if(pStack->currentElementCount == pStack->maxElementCount) return false;
     pStack->pElement[pStack->currentElementCount] = element;
     pStack->currentElementCount++;
     return true;
@@ -33,17 +32,17 @@ bool pushAS(ArrayStack* pStack, ArrayStackNode element){
 
 ArrayStackNode* popAS(ArrayStack* pStack){
     if(pStack == NULL) return NULL;
-    if(isArrayStackEmpty(pStack)) return NULL;
+    if(pStack->currentElementCount == 0) return NULL;
     ArrayStackNode* pReturn = (ArrayStackNode*)malloc(sizeof(ArrayStackNode));
     if(pReturn == NULL) return NULL;
+    *pReturn = pStack->pElement[pStack->currentElementCount - 1];
     pStack->currentElementCount--;
-    *pReturn = pStack->pElement[pStack->currentElementCount];
-    return pReturn;
+    return pReturn; 
 }
 
 ArrayStackNode* peekAS(ArrayStack* pStack){
     if(pStack == NULL) return NULL;
-    if(isArrayStackEmpty(pStack)) return NULL;
+    if(pStack->currentElementCount == 0) return NULL;
     return &(pStack->pElement[pStack->currentElementCount - 1]);
 }
 
