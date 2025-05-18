27d273bfbd18f212341b8f5845b1721929e60ddf
gus8054 <gus8054@kakao.com>
2025-05-19 02:27:18 +0900

    MODIFY: binsearchtree

--- tree/binsearchtree/binsearchtree.c
+++ tree/binsearchtree/binsearchtree.c
@@ -3,17 +3,14 @@
 BinSearchTree* createBinSearchTree(){
     BinSearchTree* pReturn = (BinSearchTree*)malloc(sizeof(BinSearchTree));
     if(pReturn == NULL) return NULL;
-   
-    pReturn->pRootNode = NULL;
 
+    pReturn->pRootNode = NULL;
     return pReturn;
 }
 
 void deleteBinSearchTree(BinSearchTree* pTree){
     if(pTree == NULL) return;
-    if(pTree->pRootNode != NULL){
-        deleteBinSearchTreeNode(pTree->pRootNode);
-    }
+    deleteBinSearchTreeNode(pTree->pRootNode);
     free(pTree);
 }
 
@@ -26,309 +23,41 @@ void deleteBinSearchTreeNode(BinSearchTreeNode* pTreeNode){
 
 bool insertElementBST(BinSearchTree* pTree, BinSearchTreeNode element){
     if(pTree == NULL) return false;
-
-    // 트리가 비어있다면 새 노드를 추가하고 종료.
-    if(pTree->pRootNode == NULL){
-        BinSearchTreeNode* pNewNode = (BinSearchTreeNode*)malloc(sizeof(BinSearchTreeNode));
-        if(pNewNode == NULL) return false;
-        *pNewNode = element;
-        pNewNode->pLeftChild = NULL;
-        pNewNode->pRightChild = NULL;
-        pTree->pRootNode = pNewNode;
-        return true;
-    }
-    BinSearchTreeNode* pParentNode = NULL;
-    BinSearchTreeNode* pCurrentNode = pTree->pRootNode;
-    // 키가 존재하는지 확인
-    while(true){
-        // 같은 노드가 존재하면 함수 종료
-        if(element.key == pCurrentNode->key) return false;
-
-        pParentNode = pCurrentNode;
-
-        if(element.key < pCurrentNode->key){
-            pCurrentNode = pCurrentNode->pLeftChild;
-        }else{
-            pCurrentNode = pCurrentNode->pRightChild;
-        }
-        // 삽입할 위치를 찾으면 반복종료.
-        if(pCurrentNode == NULL) break;
-    }
-
-    // 새 노드 추가
+    
     BinSearchTreeNode* pNewNode = (BinSearchTreeNode*)malloc(sizeof(BinSearchTreeNode));
     if(pNewNode == NULL) return false;
     *pNewNode = element;
     pNewNode->pLeftChild = NULL;
     pNewNode->pRightChild = NULL;
 
-    if(element.key < pParentNode->key){
-        pParentNode->pLeftChild = pNewNode;
-    }else{
-        pParentNode->pRightChild = pNewNode;
-    }
-    return true;
-}
-
-int getChildCount(BinSearchTreeNode* pNode) {
-    if (pNode == NULL) return -1;
-    
-    int count = 0;
-    if (pNode->pLeftChild != NULL) count++;
-    if (pNode->pRightChild != NULL) count++;
-    
-    return count;
-}
-
-/**
- * 이진 탐색 트리에서 후계자 노드를 찾는 함수 (삭제할 노드의 오른쪽 서브트리에서 가장 작은 값)
- * @param pNode 후계자를 찾을 기준 노드
- * @return 후계자 노드와 그 부모 노드
- */
-void findInOrderSuccessor(BinSearchTreeNode* pNode, BinSearchTreeNode** ppSuccessor, BinSearchTreeNode** ppParent) {
-    *ppParent = pNode;
-    *ppSuccessor = pNode->pRightChild;
-    
-    // 오른쪽 서브트리에서 가장 왼쪽(가장 작은) 노드를 찾음
-    while ((*ppSuccessor)->pLeftChild != NULL) {
-        *ppParent = *ppSuccessor;
-        *ppSuccessor = (*ppSuccessor)->pLeftChild;
-    }
-}
-
-/**
- * 이진 탐색 트리에서 요소 삭제
- * @param pTree 이진 탐색 트리
- * @param key 삭제할 요소의 키 값
- * @return 삭제 성공 여부
- */
-bool deleteElementBST(BinSearchTree* pTree, int key) {
-    // 유효성 검사
-    if (pTree == NULL || pTree->pRootNode == NULL) {
-        return false;
+    if(pTree->pRootNode == NULL){
+       pTree->pRootNode = pNewNode;
+       return true; 
     }
 
-    // 삭제할 노드와 그 부모 노드 찾기
     BinSearchTreeNode* pParentNode = NULL;
-    BinSearchTreeNode* pDelNode = pTree->pRootNode;
-    
-    // 삭제할 노드 탐색
-    while (pDelNode != NULL && pDelNode->key != key) {
-        pParentNode = pDelNode;
-        
-        if (key < pDelNode->key) {
-            pDelNode = pDelNode->pLeftChild;
-        } else {
-            pDelNode = pDelNode->pRightChild;
-        }
-    }
-    
-    // 삭제할 노드가 없으면 실패
-    if (pDelNode == NULL) {
-        return false;
-    }
-    
-    // 자식 노드 수에 따라 다른 처리
-    int childCount = getChildCount(pDelNode);
-    
-    // 경우 1: 리프 노드 (자식 없음)
-    if (childCount == 0) {
-        // 루트 노드인 경우
-        if (pParentNode == NULL) {
-            pTree->pRootNode = NULL;
-        } 
-        // 부모 노드와 연결 끊기
-        else {
-            if (pParentNode->pLeftChild == pDelNode) {
-                pParentNode->pLeftChild = NULL;
-            } else {
-                pParentNode->pRightChild = NULL;
-            }
-        }
-    }
-    // 경우 2: 자식이 하나인 경우
-    else if (childCount == 1) {
-        // 자식 노드 결정
-        BinSearchTreeNode* pChildNode = (pDelNode->pLeftChild != NULL) ? 
-                                        pDelNode->pLeftChild : pDelNode->pRightChild;
-        
-        // 루트 노드인 경우
-        if (pParentNode == NULL) {
-            pTree->pRootNode = pChildNode;
-        }
-        // 부모 노드와 자식 노드 연결
-        else {
-            if (pParentNode->pLeftChild == pDelNode) {
-                pParentNode->pLeftChild = pChildNode;
-            } else {
-                pParentNode->pRightChild = pChildNode;
-            }
-        }
-    }
-    // 경우 3: 자식이 둘인 경우
-    else {
-        // 후계자 노드 찾기 (중위 순회 기준 다음 노드)
-        BinSearchTreeNode* pSuccessor = NULL;
-        BinSearchTreeNode* pSuccessorParent = NULL;
-        
-        findInOrderSuccessor(pDelNode, &pSuccessor, &pSuccessorParent);
-        
-        // 후계자가 삭제 노드의 직접적인 자식이 아닌 경우
-        if (pSuccessorParent != pDelNode) {
-            // 후계자의 오른쪽 자식을 후계자 부모의 왼쪽에 연결
-            pSuccessorParent->pLeftChild = pSuccessor->pRightChild;
-            
-            // 후계자가 삭제 노드의 오른쪽 자식을 물려받음
-            pSuccessor->pRightChild = pDelNode->pRightChild;
-        }
-        
-        // 후계자가 삭제 노드의 왼쪽 자식을 물려받음
-        pSuccessor->pLeftChild = pDelNode->pLeftChild;
-        
-        // 삭제 노드가 루트인 경우
-        if (pParentNode == NULL) {
-            pTree->pRootNode = pSuccessor;
-        }
-        // 부모 노드와 후계자 연결
-        else {
-            if (pParentNode->pLeftChild == pDelNode) {
-                pParentNode->pLeftChild = pSuccessor;
-            } else {
-                pParentNode->pRightChild = pSuccessor;
-            }
-        }
-    }
-    
-    // 삭제 노드 메모리 해제
-    free(pDelNode);
-    return true;
-}
+    BinSearchTreeNode* pTreeNode = pTree->pRootNode;
 
-int getChildCountBST(BinSearchTreeNode* pNode) {
-    if (pNode == NULL) return -1;
-    
-    int count = 0;
-    if (pNode->pLeftChild != NULL) count++;
-    if (pNode->pRightChild != NULL) count++;
-    
-    return count;
-}
-
-void findSuccessor(BinSearchTreeNode* pNode, BinSearchTreeNode** ppSuccessor, BinSearchTreeNode** ppParent) {
-    *ppParent = pNode;
-    *ppSuccessor = pNode->pLeftChild;
-    // *ppSuccessor = pNode->pRightChild;
-    
-    // 오른쪽 서브트리에서 가장 왼쪽(가장 작은) 노드를 찾음
-    // while ((*ppSuccessor)->pLeftChild != NULL) {
-    //     *ppParent = *ppSuccessor;
-    //     *ppSuccessor = (*ppSuccessor)->pLeftChild;
-    // }
-    while((*ppSuccessor)->pRightChild != NULL){
-        *ppParent = *ppSuccessor;
-        *ppSuccessor = (*ppSuccessor)->pRightChild;
-    }
-}
-
-bool removeElementBST(BinSearchTree* pTree, int key) {
-    // 유효성 검사
-    if (pTree == NULL || pTree->pRootNode == NULL) {
-        return false;
-    }
-
-    // 삭제할 노드와 그 부모 노드 찾기
-    BinSearchTreeNode* pParentNode = NULL;
-    BinSearchTreeNode* pDelNode = pTree->pRootNode;
-    
-    // 삭제할 노드 탐색
-    while (pDelNode != NULL && pDelNode->key != key) {
-        pParentNode = pDelNode;
-        
-        if (key < pDelNode->key) {
-            pDelNode = pDelNode->pLeftChild;
-        } else {
-            pDelNode = pDelNode->pRightChild;
-        }
-    }
-    
-    // 삭제할 노드가 없으면 실패
-    if (pDelNode == NULL) {
-        return false;
-    }
-    
-    // 자식 노드 수에 따라 다른 처리
-    int childCount = getChildCountBST(pDelNode);
-    
-    // 경우 1: 리프 노드 (자식 없음)
-    if (childCount == 0) {
-        // 루트 노드인 경우
-        if (pParentNode == NULL) {
-            pTree->pRootNode = NULL;
-        } 
-        // 부모 노드와 연결 끊기
-        else {
-            if (pParentNode->pLeftChild == pDelNode) {
-                pParentNode->pLeftChild = NULL;
-            } else {
-                pParentNode->pRightChild = NULL;
+    while(true){
+        if(element.key < pTreeNode->key){
+            pParentNode = pTreeNode;
+            pTreeNode = pTreeNode->pLeftChild;
+            if(pTreeNode == NULL){
+                pParentNode->pLeftChild = pNewNode;
+                break;
+            } 
+        }else if(element.key > pTreeNode->key){
+            pParentNode = pTreeNode;
+            pTreeNode = pTreeNode->pRightChild;
+            if(pTreeNode == NULL){
+                pParentNode->pRightChild = pNewNode;
+                break;
             }
+        }else{
+            free(pNewNode);
+            return false;
         }
     }
-    // 경우 2: 자식이 하나인 경우
-    else if (childCount == 1) {
-        // 자식 노드 결정
-        BinSearchTreeNode* pChildNode = (pDelNode->pLeftChild != NULL) ? 
-                                        pDelNode->pLeftChild : pDelNode->pRightChild;
-        
-        // 루트 노드인 경우
-        if (pParentNode == NULL) {
-            pTree->pRootNode = pChildNode;
-        }
-        // 부모 노드와 자식 노드 연결
-        else {
-            if (pParentNode->pLeftChild == pDelNode) {
-                pParentNode->pLeftChild = pChildNode;
-            } else {
-                pParentNode->pRightChild = pChildNode;
-            }
-        }
-    }
-    // 경우 3: 자식이 둘인 경우
-    else {
-        // 후계자 노드 찾기 (중위 순회 기준 다음 노드)
-        BinSearchTreeNode* pSuccessor = NULL;
-        BinSearchTreeNode* pSuccessorParent = NULL;
-        
-        findSuccessor(pDelNode, &pSuccessor, &pSuccessorParent);
-        
-        // 후계자가 삭제 노드의 직접적인 자식이 아닌 경우
-        if (pSuccessorParent != pDelNode) {
-            // 후계자의 오른쪽 자식을 후계자 부모의 왼쪽에 연결
-            pSuccessorParent->pLeftChild = pSuccessor->pRightChild;
-            
-            // 후계자가 삭제 노드의 오른쪽 자식을 물려받음
-            pSuccessor->pRightChild = pDelNode->pRightChild;
-        }
-        
-        // 후계자가 삭제 노드의 왼쪽 자식을 물려받음
-        pSuccessor->pLeftChild = pDelNode->pLeftChild;
-        
-        // 삭제 노드가 루트인 경우
-        if (pParentNode == NULL) {
-            pTree->pRootNode = pSuccessor;
-        }
-        // 부모 노드와 후계자 연결
-        else {
-            if (pParentNode->pLeftChild == pDelNode) {
-                pParentNode->pLeftChild = pSuccessor;
-            } else {
-                pParentNode->pRightChild = pSuccessor;
-            }
-        }
-    }
-    
-    // 삭제 노드 메모리 해제
-    free(pDelNode);
     return true;
 }
 
@@ -336,18 +65,125 @@ BinSearchTreeNode* searchBST(BinSearchTree* pTree, int key){
     if(pTree == NULL) return NULL;
     if(pTree->pRootNode == NULL) return NULL;
 
-    BinSearchTreeNode* pCurrentNode = pTree->pRootNode;
+    BinSearchTreeNode* pTreeNode = pTree->pRootNode;
     while(true){
-        if(pCurrentNode == NULL) return NULL;
-        if(key == pCurrentNode->key) break;
-        else if(key < pCurrentNode->key){
-            pCurrentNode = pCurrentNode->pLeftChild;
+        if(key < pTreeNode->key){
+            pTreeNode = pTreeNode->pLeftChild;
+        }else if(key > pTreeNode->key){
+            pTreeNode = pTreeNode->pRightChild;
         }else{
-            pCurrentNode = pCurrentNode->pRightChild;
+            break;
         }
+        if(pTreeNode == NULL) return NULL;
     }
-
-    return pCurrentNode;
+    return pTreeNode;
 }
 
+bool findRemoveNode(BinSearchTree* pTree, int key, BinSearchTreeNode** ppParentNode, BinSearchTreeNode** ppChildNode){
+    if(pTree == NULL) return false;
+    if(pTree->pRootNode == NULL) return false;
 
+    *ppParentNode = NULL;
+    *ppChildNode = pTree->pRootNode;
+
+    while(*ppChildNode != NULL){
+        if(key < (*ppChildNode)->key){
+            *ppParentNode = *ppChildNode;
+            *ppChildNode = (*ppChildNode)->pLeftChild;
+        }else if(key > (*ppChildNode)->key){
+            *ppParentNode = *ppChildNode;
+            *ppChildNode = (*ppChildNode)->pRightChild;
+        }else break;
+    }
+    if(*ppChildNode == NULL) return false;
+    return true;
+}
+
+bool removeElementBST(BinSearchTree* pTree, int key){
+    if(pTree == NULL) return false;
+    if(pTree->pRootNode == NULL) return false;
+
+    BinSearchTreeNode* pParentNode = NULL; // 삭제할 노드의 부모 노드
+    BinSearchTreeNode* pChildNode = pTree->pRootNode; // 삭제할 노드
+
+    if(!findRemoveNode(pTree, key, &pParentNode, &pChildNode)) return false;
+
+    // 삭제할 노드가 루트 노드일 때
+    if(pParentNode == NULL){
+        // 삭제할 노드가 리프노드 일 때
+        if(pChildNode->pLeftChild == NULL && pChildNode->pRightChild == NULL){
+            pTree->pRootNode = NULL;
+        }else if(pChildNode->pLeftChild != NULL && pChildNode->pRightChild != NULL){
+            // 삭제할 노드가 두 개의 자식을 가질 때
+            BinSearchTreeNode* pPreNode = pChildNode;
+            BinSearchTreeNode* pCurrentNode = pChildNode->pRightChild;
+            while(true){
+                if(pCurrentNode->pLeftChild == NULL) break;
+                pPreNode = pCurrentNode;
+                pCurrentNode = pCurrentNode->pLeftChild; 
+            }
+            if(pPreNode == pChildNode){
+                pPreNode->pRightChild = pCurrentNode->pRightChild;
+            }else{
+                pPreNode->pLeftChild = pCurrentNode->pRightChild;
+            }
+            pCurrentNode->pLeftChild = pChildNode->pLeftChild;
+            pCurrentNode->pRightChild = pChildNode->pRightChild;
+            pTree->pRootNode = pCurrentNode;
+        }else if(pChildNode->pLeftChild != NULL){
+            // 삭제할 노드가 왼쪽 자식만 가질 때
+            pTree->pRootNode = pChildNode->pLeftChild;
+        }else{
+            // 삭제할 노드가 오른쪽 자식만 가질 때
+            pTree->pRootNode = pChildNode->pRightChild;
+        }
+        free(pChildNode);
+    }else{
+    // 삭제할 노드가 루트 노드가 아닐 때
+        // 삭제할 노드가 리프노드 일 때
+        if(pChildNode->pLeftChild == NULL && pChildNode->pRightChild == NULL){
+            if(pParentNode->pLeftChild == pChildNode){
+                pParentNode->pLeftChild = NULL;
+            }else{
+                pParentNode->pRightChild = NULL;
+            }
+        }else if(pChildNode->pLeftChild != NULL && pChildNode->pRightChild != NULL){
+            // 삭제할 노드가 두 개의 자식을 가질 때
+            BinSearchTreeNode* pPreNode = pChildNode;
+            BinSearchTreeNode* pCurrentNode = pChildNode->pRightChild;
+            while(true){
+                if(pCurrentNode->pLeftChild == NULL) break;
+                pPreNode = pCurrentNode;
+                pCurrentNode = pCurrentNode->pLeftChild; 
+            }
+            if(pPreNode == pChildNode){
+                pPreNode->pRightChild = pCurrentNode->pRightChild;
+            }else{
+                pPreNode->pLeftChild = pCurrentNode->pRightChild;
+            }
+            pCurrentNode->pLeftChild = pChildNode->pLeftChild;
+            pCurrentNode->pRightChild = pChildNode->pRightChild;
+            if(pParentNode->pLeftChild == pChildNode){
+                pParentNode->pLeftChild = pCurrentNode;
+            }else{
+                pParentNode->pRightChild = pCurrentNode;
+            }
+        }else if(pChildNode->pLeftChild != NULL){
+            // 삭제할 노드가 왼쪽 자식만 가질 때
+            if(pParentNode->pLeftChild == pChildNode){
+                pParentNode->pLeftChild = pChildNode->pLeftChild;
+            }else{
+                pParentNode->pRightChild = pChildNode->pLeftChild;
+            }
+        }else{
+            // 삭제할 노드가 오른쪽 자식만 가질 때
+            if(pParentNode->pLeftChild == pChildNode){
+                pParentNode->pLeftChild = pChildNode->pRightChild;
+            }else{
+                pParentNode->pRightChild = pChildNode->pRightChild;
+            }
+        }
+        free(pChildNode);
+    }
+    return true;
+}
--- tree/binsearchtree/binsearchtreetest.c
+++ tree/binsearchtree/binsearchtreetest.c
@@ -1,6 +1,6 @@
 #include "binsearchtree.c"
 
-// 중위 순회 함수 추가 (테스트용)
+// 추가 함수: 중위 순회로 트리 출력
 void inorderTraversal(BinSearchTreeNode* pNode) {
     if (pNode != NULL) {
         inorderTraversal(pNode->pLeftChild);
@@ -9,233 +9,133 @@ void inorderTraversal(BinSearchTreeNode* pNode) {
     }
 }
 
-// 트리 상태 출력 함수
-void displayTree(BinSearchTree* pTree) {
-    printf("\n=== 이진 탐색 트리 상태 ===\n");
-    if (pTree == NULL || pTree->pRootNode == NULL) {
-        printf("트리가 비어 있습니다.\n");
-        return;
-    }
+// 추가 함수: 트리 구조 시각적 출력
+void printTree(BinSearchTreeNode* root, int level) {
+    if (root == NULL) return;
     
-    inorderTraversal(pTree->pRootNode);
-    printf("=======================\n");
+    printTree(root->pRightChild, level + 1);
+    
+    for (int i = 0; i < level; i++) printf("    ");
+    printf("%d(%c)\n", root->key, root->value);
+    
+    printTree(root->pLeftChild, level + 1);
 }
 
-// 테스트 케이스 함수들
-void testInsert(BinSearchTree* pTree) {
-    printf("\n[삽입 테스트 시작]\n");
-    
-    BinSearchTreeNode nodes[] = {
-        {'A', 30, NULL, NULL},
-        {'B', 20, NULL, NULL},
-        {'C', 40, NULL, NULL},
-        {'D', 10, NULL, NULL},
-        {'E', 25, NULL, NULL},
-        {'F', 35, NULL, NULL},
-        {'G', 50, NULL, NULL}
-    };
-    
-    for (int i = 0; i < 7; i++) {
-        if (insertElementBST(pTree, nodes[i])) {
-            printf("삽입 성공 - 키: %d, 값: %c\n", nodes[i].key, nodes[i].value);
-        } else {
-            printf("삽입 실패 - 키: %d, 값: %c\n", nodes[i].key, nodes[i].value);
-        }
-    }
-    
-    // 중복 키 삽입 테스트
-    BinSearchTreeNode duplicateNode = {'X', 30, NULL, NULL};
-    if (!insertElementBST(pTree, duplicateNode)) {
-        printf("중복 키 삽입 거부 테스트 성공 - 키: %d\n", duplicateNode.key);
-    } else {
-        printf("중복 키 삽입 거부 테스트 실패 - 키: %d\n", duplicateNode.key);
-    }
-    
-    displayTree(pTree);
-    printf("[삽입 테스트 종료]\n");
-}
-
-void testSearch(BinSearchTree* pTree) {
-    printf("\n[검색 테스트 시작]\n");
-    
-    int keysToSearch[] = {30, 20, 40, 10, 25, 35, 50, 15, 60};
-    
-    for (int i = 0; i < 9; i++) {
-        BinSearchTreeNode* foundNode = searchBST(pTree, keysToSearch[i]);
-        if (foundNode != NULL) {
-            printf("키 %d 검색 성공 - 값: %c\n", keysToSearch[i], foundNode->value);
-        } else {
-            printf("키 %d 검색 실패 - 존재하지 않음\n", keysToSearch[i]);
-        }
-    }
-    
-    printf("[검색 테스트 종료]\n");
-}
-
-void testDelete(BinSearchTree* pTree) {
-    printf("\n[삭제 테스트 시작]\n");
-    
-    // 1. 리프 노드 삭제
-    printf("1. 리프 노드 삭제 (키: 10)\n");
-    if (deleteElementBST(pTree, 10)) {
-        printf("키 10 삭제 성공\n");
-    } else {
-        printf("키 10 삭제 실패\n");
-    }
-    displayTree(pTree);
-    
-    // 2. 자식이 하나인 노드 삭제
-    printf("\n2. 자식이 하나인 노드 삭제 (키: 40)\n");
-    if (deleteElementBST(pTree, 40)) {
-        printf("키 40 삭제 성공\n");
-    } else {
-        printf("키 40 삭제 실패\n");
-    }
-    displayTree(pTree);
-    
-    // 3. 자식이 둘인 노드 삭제
-    printf("\n3. 자식이 둘인 노드 삭제 (키: 20)\n");
-    if (deleteElementBST(pTree, 20)) {
-        printf("키 20 삭제 성공\n");
-    } else {
-        printf("키 20 삭제 실패\n");
-    }
-    displayTree(pTree);
-    
-    // 4. 루트 노드 삭제
-    printf("\n4. 루트 노드 삭제 (키: 30)\n");
-    if (deleteElementBST(pTree, 30)) {
-        printf("키 30 삭제 성공\n");
-    } else {
-        printf("키 30 삭제 실패\n");
-    }
-    displayTree(pTree);
-    
-    // 5. 존재하지 않는 키 삭제 시도
-    printf("\n5. 존재하지 않는 키 삭제 시도 (키: 100)\n");
-    if (!deleteElementBST(pTree, 100)) {
-        printf("존재하지 않는 키 삭제 거부 테스트 성공\n");
-    } else {
-        printf("존재하지 않는 키 삭제 거부 테스트 실패\n");
-    }
-    
-    printf("[삭제 테스트 종료]\n");
-}
-
-// 추가: 트리 구조 출력 함수 (디버깅용)
-void printTree(BinSearchTreeNode* node, int level) {
-    if (node != NULL) {
-        // 오른쪽 서브트리 먼저 출력 (그래야 시각적으로 왼쪽이 왼쪽에 표시됨)
-        printTree(node->pRightChild, level + 1);
-        
-        // 현재 노드 출력
-        for (int i = 0; i < level; i++) {
-            printf("    ");
-        }
-        printf("[%d:%c]\n", node->key, node->value);
-        
-        // 왼쪽 서브트리 출력
-        printTree(node->pLeftChild, level + 1);
-    }
-}
-
-// 중위 순회로 트리 내용 출력 (오름차순)
-void inOrder(BinSearchTreeNode* node) {
-    if (node != NULL) {
-        inOrder(node->pLeftChild);
-        printf("%d:%c ", node->key, node->value);
-        inOrder(node->pRightChild);
-    }
-}
-
+// 메인 함수 - 테스트 코드
 int binSearchTreeMain() {
-    printf("=== 이진 탐색 트리 테스트 시작 ===\n");
-    
-    // 트리 생성
+    // 이진 탐색 트리 생성
     BinSearchTree* myTree = createBinSearchTree();
     if (myTree == NULL) {
         printf("트리 생성 실패\n");
         return -1;
     }
-    printf("트리 생성 성공\n");
+    printf("이진 탐색 트리 생성 성공\n\n");
     
-    // 삽입 테스트
-    testInsert(myTree);
-    
-    // 검색 테스트
-    testSearch(myTree);
-    
-    // 삭제 테스트
-    testDelete(myTree);
-    
-    // 메모리 해제
-    deleteBinSearchTree(myTree);
-    printf("\n트리 메모리 해제 완료\n");
-    
-    printf("=== 이진 탐색 트리 테스트 종료 ===\n");
-    
-    printf("\n삭제 노드의 왼쪽자식이 오른쪽 자식을 갖지 않는 케이스 테이스 시작\n");
-
-    BinSearchTree* pTree = createBinSearchTree();
-    if (pTree == NULL) {
-        printf("트리 생성 실패\n");
-        return -1;
-    }
-    
-    // 테스트 데이터 준비
+    // 노드 삽입 테스트
     BinSearchTreeNode nodes[] = {
-        {'A', 30, NULL, NULL},  // 루트 노드
-        {'B', 20, NULL, NULL},  // 루트의 왼쪽 자식
-        {'C', 40, NULL, NULL},  // 루트의 오른쪽 자식
-        {'D', 10, NULL, NULL},  // B의 왼쪽 자식
+        {'G', 7, NULL, NULL},  // 루트 노드
+        {'D', 4, NULL, NULL},
+        {'B', 2, NULL, NULL},
+        {'F', 6, NULL, NULL},
+        {'A', 1, NULL, NULL},
+        {'C', 3, NULL, NULL},
+        {'E', 5, NULL, NULL},
+        {'I', 9, NULL, NULL},
+        {'H', 8, NULL, NULL},
+        {'J', 10, NULL, NULL}
     };
     
-    // 트리에 노드 삽입
+    printf("=== 노드 삽입 테스트 ===\n");
     for (int i = 0; i < sizeof(nodes)/sizeof(nodes[0]); i++) {
-        if (!insertElementBST(pTree, nodes[i])) {
-            printf("노드 삽입 실패: %d\n", nodes[i].key);
+        if (insertElementBST(myTree, nodes[i])) {
+            printf("키:%d, 값:%c 삽입 성공\n", nodes[i].key, nodes[i].value);
+        } else {
+            printf("키:%d, 값:%c 삽입 실패\n", nodes[i].key, nodes[i].value);
         }
     }
     
-    // 초기 트리 상태 출력
-    printf("===== 초기 트리 상태 =====\n");
-    printTree(pTree->pRootNode, 0);
-    printf("\n중위 순회 결과: ");
-    inOrder(pTree->pRootNode);
-    printf("\n\n");
-    
-    // 문제가 발생하는 케이스: 루트 노드(30) 삭제
-    printf("===== 노드 30 삭제 =====\n");
-    if (deleteElementBST(pTree, 30)) {
-        printf("노드 30 삭제 성공\n");
-    } else {
-        printf("노드 30 삭제 실패\n");
+    // 이미 존재하는 키로 삽입 테스트
+    BinSearchTreeNode duplicateNode = {'X', 7, NULL, NULL};
+    printf("\n=== 중복 키 삽입 테스트 ===\n");
+    if (!insertElementBST(myTree, duplicateNode)) {
+        printf("키:%d 중복으로 삽입 실패 (정상)\n", duplicateNode.key);
     }
     
-    // 삭제 후 트리 상태 출력 (오류 발생하면 잘못된 트리 구조나 무한 루프 발생)
-    printf("\n===== 삭제 후 트리 상태 =====\n");
-    printTree(pTree->pRootNode, 0);
-    printf("\n중위 순회 결과: ");
-    inOrder(pTree->pRootNode);  // 순환 참조 있으면 스택 오버플로우 발생
-    printf("\n\n");
+    // 트리 출력 (중위 순회)
+    printf("\n=== 트리 중위 순회 (오름차순 출력) ===\n");
+    inorderTraversal(myTree->pRootNode);
     
-    // 추가 테스트: 노드 20 삭제
-    // 이것도 문제 발생할 수 있음 (왼쪽 자식이 자기 자신 참조)
-    printf("===== 노드 20 삭제 =====\n");
-    if (deleteElementBST(pTree, 20)) {
-        printf("노드 20 삭제 성공\n");
-    } else {
-        printf("노드 20 삭제 실패\n");
+    // 트리 구조 시각적 출력
+    printf("\n=== 트리 구조 ===\n");
+    printTree(myTree->pRootNode, 0);
+    
+    // 검색 테스트
+    printf("\n=== 검색 테스트 ===\n");
+    int keysToSearch[] = {5, 3, 11};
+    for (int i = 0; i < sizeof(keysToSearch)/sizeof(keysToSearch[0]); i++) {
+        BinSearchTreeNode* foundNode = searchBST(myTree, keysToSearch[i]);
+        if (foundNode != NULL) {
+            printf("키:%d 검색 성공 - 값:%c\n", keysToSearch[i], foundNode->value);
+        } else {
+            printf("키:%d 검색 실패 - 존재하지 않음\n", keysToSearch[i]);
+        }
     }
     
-    printf("\n===== 삭제 후 최종 트리 상태 =====\n");
-    printTree(pTree->pRootNode, 0);
-    printf("\n중위 순회 결과: ");
-    inOrder(pTree->pRootNode);
-    printf("\n\n");
+    // 삭제 테스트 - 리프 노드 삭제
+    printf("\n=== 리프 노드 삭제 테스트 ===\n");
+    int leafKey = 1; // 'A' 노드 (리프 노드)
+    if (removeElementBST(myTree, leafKey)) {
+        printf("키:%d 삭제 성공\n", leafKey);
+    } else {
+        printf("키:%d 삭제 실패\n", leafKey);
+    }
+    
+    printf("\n=== 리프 노드 삭제 후 트리 구조 ===\n");
+    printTree(myTree->pRootNode, 0);
+    
+    // 삭제 테스트 - 자식이 하나인 노드 삭제
+    printf("\n=== 자식이 하나인 노드 삭제 테스트 ===\n");
+    int oneChildKey = 9; // 'I' 노드 (자식이 하나)
+    if (removeElementBST(myTree, oneChildKey)) {
+        printf("키:%d 삭제 성공\n", oneChildKey);
+    } else {
+        printf("키:%d 삭제 실패\n", oneChildKey);
+    }
+    
+    printf("\n=== 자식이 하나인 노드 삭제 후 트리 구조 ===\n");
+    printTree(myTree->pRootNode, 0);
+    
+    // 삭제 테스트 - 자식이 둘인 노드 삭제
+    printf("\n=== 자식이 둘인 노드 삭제 테스트 ===\n");
+    int twoChildrenKey = 4; // 'D' 노드 (자식이 둘)
+    if (removeElementBST(myTree, twoChildrenKey)) {
+        printf("키:%d 삭제 성공\n", twoChildrenKey);
+    } else {
+        printf("키:%d 삭제 실패\n", twoChildrenKey);
+    }
+    
+    printf("\n=== 자식이 둘인 노드 삭제 후 트리 구조 ===\n");
+    printTree(myTree->pRootNode, 0);
+    
+    // 루트 노드 삭제 테스트
+    printf("\n=== 루트 노드 삭제 테스트 ===\n");
+    int rootKey = 7; // 'G' 노드 (루트 노드)
+    if (removeElementBST(myTree, rootKey)) {
+        printf("키:%d (루트 노드) 삭제 성공\n", rootKey);
+    } else {
+        printf("키:%d 삭제 실패\n", rootKey);
+    }
+    
+    printf("\n=== 루트 노드 삭제 후 트리 구조 ===\n");
+    printTree(myTree->pRootNode, 0);
+    
+    // 트리가 비어있는지 확인
+    printf("\n=== 최종 중위 순회 ===\n");
+    inorderTraversal(myTree->pRootNode);
     
     // 트리 메모리 해제
-    deleteBinSearchTree(pTree);
-    printf("트리 메모리 해제 완료\n");
+    deleteBinSearchTree(myTree);
+    printf("\n이진 탐색 트리 메모리 해제 완료\n");
+    
     return 0;
 }
